49164fa796d56d07c288c146fb496432979fc4d9
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-12-18 13:49:30 +0900

    - fix build - unity build 개선

--- Engine/Source/CommonProject.build.cs
+++ Engine/Source/CommonProject.build.cs
@@ -46,6 +46,7 @@ public abstract class CommonProject : Project
         }
         conf.Options.Add(Options.Vc.General.CharacterSet.Unicode);
         conf.Options.Add(Options.Vc.Compiler.JumboBuild.Enable);
+        conf.MinFilesPerJumboFile = 100;
         conf.Options.Add(Options.Vc.Compiler.CppLanguageStandard.CPP20);
         //conf.Options.Add(Options.Vc.Compiler.CppLanguageStandard.Latest);
         if (target.Optimization == Optimization.Debug)
